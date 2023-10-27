80017c0f1714f1a65284072029208bbf48a0821f
Ouatar Hakim <hakimouatar6@gmail.com>
2023-10-27 14:44:53 +0200

    config screen navigation

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,23 @@
-import { StripeProvider } from '@stripe/stripe-react-native';
-import Constants from 'expo-constants';
-import CheckoutScreen from './CheckoutScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import HomeScreen from "./routes/HomeScreen";
+import ScannerScreen from "./routes/ScannerScreen";
+import ShoppingCart from "./routes/ShoppingCart";
+const Stack = createNativeStackNavigator();
+
+/**
+ * Fichier Racine du projet App.
+ * @constructor
+ */
 export default function App() {
-  const stripePK = Constants.expoConfig.extra.stripePK;
-
   return (
-    <StripeProvider
-      publishableKey={stripePK}
-      merchantIdentifier="merchant.com.example"
-    >
-      <CheckoutScreen />
-    </StripeProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Accueil" component={HomeScreen} />
+        <Stack.Screen name="Scanner" component={ScannerScreen} />
+        <Stack.Screen name="Panier" component={ShoppingCart} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
