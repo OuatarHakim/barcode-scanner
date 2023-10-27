e5cb7bb121fc238c2176a175f4ea10c0bda4dde4
Ouatar Hakim <hakimouatar6@gmail.com>
2023-10-27 14:44:20 +0200

    create ShoppingCart Screen

---
+++ client/routes/ShoppingCart.tsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import Menu from "../components/Menu";
+
+export default function ShoppingCart({ navigation }: any) {
+  return <Menu />;
+}
