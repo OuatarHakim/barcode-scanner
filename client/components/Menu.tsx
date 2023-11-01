27bfb8c187114bb89861f179457eaa50b9e421ab
Ouatar Hakim <hakimouatar6@gmail.com>
2023-11-01 17:07:30 +0100

    refactoring icon and menu

--- client/components/Menu.tsx
+++ client/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { View, StyleSheet } from "react-native";
 import MenuIcon from "./MenuIcon";
 import { useNavigation } from "@react-navigation/native";
@@ -34,6 +34,64 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
+  },
+  iconContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+});
+
+export default Menu;*/
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MenuIcon from "./MenuIcon";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+const Menu = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <MenuIcon
+          iconName="home"
+          text=""
+          onPress={() => navigation.navigate("Accueil")}
+          isHighlighted={route.name === "Accueil"}
+        />
+        <MenuIcon
+          iconName="qrcode-scan"
+          text=""
+          onPress={() => navigation.navigate("Scanner")}
+          isHighlighted={route.name === "Scanner"}
+        />
+        <MenuIcon
+          iconName="history"
+          text=""
+          onPress={() => navigation.navigate("Historique")}
+          isHighlighted={route.name === "Historique"}
+        />
+        <MenuIcon
+          iconName="cart"
+          text=""
+          onPress={() => navigation.navigate("Panier")}
+          isHighlighted={route.name === "Panier"}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 0,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    width: "100%",
   },
   iconContainer: {
     flexDirection: "row",
--- client/components/MenuIcon.tsx
+++ client/components/MenuIcon.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
@@ -12,7 +12,7 @@ const MenuIcon: React.FC<MenuIconProps> = ({ icon, text, onPress }) => {
   return (
     <TouchableOpacity onPress={onPress} style={styles.iconButton}>
       <View style={styles.iconContainer}>
-        <FontAwesome name={icon} size={40} color="#009f89" />
+        <FontAwesome name={icon} size={40} color="#00dd00" />
       </View>
     </TouchableOpacity>
   );
@@ -25,6 +25,47 @@ const styles = StyleSheet.create({
   },
   iconText: {
     marginTop: 10,
+  },
+  iconButton: {
+    alignItems: "center",
+  },
+});
+
+export default MenuIcon;*/
+
+import React from "react";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+interface MenuIconProps {
+  iconName: string;
+  text: string;
+  onPress: () => void;
+  isHighlighted: boolean;
+}
+
+const MenuIcon: React.FC<MenuIconProps> = ({
+  iconName,
+  text,
+  onPress,
+  isHighlighted,
+}) => {
+  const iconColor = isHighlighted ? "#00dd00" : "gray";
+
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.iconButton}>
+      <View style={styles.iconContainer}>
+        <MaterialCommunityIcons name={iconName} size={30} color={iconColor} />
+        <Text style={{ color: iconColor }}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    alignItems: "center",
+    marginTop: 20,
   },
   iconButton: {
     alignItems: "center",
